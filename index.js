2c8f346a420bd5c4db075626cc37de6a36f40b21
Sérgio <71034184+Cangaceiro15@users.noreply.github.com>
2021-02-16 15:06:18 -0300

    Update: server, index, axios

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const deleteTeste = require('./src/database/deleteTeste');
 const saveTeste = require('./src/database/saveTeste');
 const consultarTeste = require('./src/database/consultarTeste');
 
-const axiosMiddleWare = require('./axios');
+const getDataFromApi = require('./axios');
 
-//deleteTeste(1);
-//consultarTeste();
+deleteTeste(18);
+consultarTeste();
 
-axiosMiddleWare().then(response => {
-  console.log(response.coord);
-  saveTeste({value: response.coord.lon})
-})
+// getDataFromApi().then(response => {
+//   console.log(response.coord);
+//   saveTeste({value: "fejaoaaa"})
+// })
--- server.js
+++ server.js
@@ -1,9 +1,6 @@
-const axiosMiddleWare = require('./axios');
 const express = require('express');
 
 const server = express();
 server.use(express.json());
-
-server.get('/', axiosMiddleWare);
 
 server.listen(3000);
